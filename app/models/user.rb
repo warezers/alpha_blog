fce3e0177c49f68b7221b003cdac85dc71f50d98
vsmmani <vsmmani@yahoo.com>
2017-10-22 16:53:51 +0900

    add secure password migration and gem add

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ActiveRecord::Base
             uniqueness: {case_sensitive: false},
             length: {minimum: 3, maximum: 25},
             format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
