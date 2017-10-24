a33c8f30d69c31400467e5f40fc3897df7b66539
vsmmani <vsmmani@yahoo.com>
2017-10-24 22:04:49 +0900

    user edit adn update control & view edit users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
 
 
   def edit
-
+    
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+ def edit
+   @user = User.find(params[:id])
+ end
+ def update
+   @user = User.find(params[:id])
+   if @user.update(user_params)
+     flash[:success] = "user details updated"
+     redirect_to articles_path
+   else
+     render 'edit'
+   end
+ end
   private
   def user_params
-    params.require(@user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
