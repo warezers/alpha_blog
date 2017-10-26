384aa89b9450be48d3a80e68321e6b214ec1428d
vsmmani <vsmmani@yahoo.com>
2017-10-26 21:57:35 +0900

    adding user image and user articles show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,10 @@ class UsersController < ApplicationController
      render 'edit'
    end
  end
+
+ def show
+   @user = User.find(params[:id])
+ end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
